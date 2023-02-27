7cce975d608784ba6664727d8db6c237a655105a
Asbjørn Kofoed-Nielsen <askn@jobindex.dk>
2023-02-27 20:48:59 +0100

    Webbrowser deployment!

--- installer/init.py
+++ installer/init.py
@@ -1,17 +1,18 @@
 import os
 import stat
+import shutil
 
 service_dst = '/etc/systemd/system/solar-controller.service'
 symlink_dst = '/etc/systemd/system/multi-user.target.wants/solar-controller.service'
 
 # move files to the root system
-os.replace('/boot/soler-controller.service', service_dst)
+shutil.copy('/boot/solar-controller.service', service_dst)
 if not os.path.isfile(symlink_dst):
     os.symlink(service_dst, symlink_dst)
-os.replace('/boot/soler-controller', '/bin/solar-controller')
+shutil.copy('/boot/solar-controller', '/bin/solar-controller')
 # make solar-controller binary executable
 st = os.stat('/bin/solar-controller')
 os.chmod('/bin/solar-controller', st.st_mode | stat.S_IEXEC)
-os.mkdirs('/var/lib/solar-controller/', exists_ok=True)
+os.makedirs('/var/lib/solar-controller/', exist_ok=True)
 
 # remove init= from cmdline.txt
